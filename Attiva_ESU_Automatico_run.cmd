2b3a74bf8e8de5cd154dab8428079739bbba118b
8emezzo <8emezzo@gmail.com>
2025-09-01 17:32:28 +0200

    Delete Attiva_ESU_Automatico_run.cmd

--- Attiva_ESU_Automatico_run.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set "_PSf=%~dp0Attiva_ESU_Automatico.ps1"
-setlocal EnableDelayedExpansion
-set "_PSf=!_PSf:'=''!"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^& "'!_PSf!' %*"
